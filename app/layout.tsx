85efb5be7c47d199746ba3ea628d46b9733e70fa
ladyprowess <okaforpeacee@gmail.com>
2026-02-10 16:05:28 +0100

    google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script"; // 👈 ADD THIS
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
 import { NotificationBar } from "@/components/NotificationBar";
@@ -12,6 +13,22 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Ads tag */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17944092598"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads-gtag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17944092598');
+          `}
+        </Script>
+      </head>
+
       <body className="min-h-screen flex flex-col overflow-x-hidden relative">
         {/* Purple background on all pages EXCEPT homepage */}
         <ConditionalBackground />
